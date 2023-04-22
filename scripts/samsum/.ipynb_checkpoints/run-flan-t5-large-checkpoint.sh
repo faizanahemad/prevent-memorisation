1534ceca8080e0455de5e4543b37f6aff03fbf34
Faizan Ahemad <ahemf@dev-dsk-ahemf-2b-7b50384b.us-west-2.amazon.com>
2023-04-22 17:28:20 +0000

    Add epoch and investigate NB

--- scripts/samsum/.ipynb_checkpoints/run-flan-t5-large-checkpoint.sh
+++ scripts/samsum/.ipynb_checkpoints/run-flan-t5-large-checkpoint.sh
@@ -2,13 +2,14 @@ MODEL="google/flan-t5-large"
 bs=12
 dataset="samsum"
 gradient_accumulation_steps=4
+baseline_epochs=10
 epochs=15
 combined_epochs=10
 num_warmup_steps=150
 proba_column="proba_v3"
 N_FOLDS=2
 
-scripts/run-baseline.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${epochs} ${num_warmup_steps}
+scripts/run-baseline.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${baseline_epochs} ${num_warmup_steps}
 scripts/run-fractional.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${epochs} ${num_warmup_steps} ${N_FOLDS} 0
 scripts/run-fractional.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${epochs} ${num_warmup_steps} ${N_FOLDS} 1
 python scripts/combining_dataset_folds.py ${MODEL} ${dataset} ${N_FOLDS}
