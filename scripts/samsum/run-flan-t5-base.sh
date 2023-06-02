97e956b30dff8fa4a44da6d33f9ab699ad1264d8
Faizan Ahemad <ahemf@dev-dsk-ahemf-p4d-acb1be10.eu-west-1.amazon.com>
2023-06-02 08:20:53 +0000

    separate fractional epochs

--- scripts/samsum/run-flan-t5-base.sh
+++ scripts/samsum/run-flan-t5-base.sh
@@ -6,6 +6,7 @@ dataset="samsum"
 gradient_accumulation_steps=4
 baseline_epochs=12
 epochs=15
+fractional_epochs=20
 combined_epochs=12
 num_warmup_steps=240
 N_FOLDS=2
@@ -16,8 +17,8 @@ seed=37
 
 
 scripts/run-baseline.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${baseline_epochs} ${num_warmup_steps} ${additional_args} ${lr} ${weight_decay} ${seed}
-scripts/run-fractional.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${epochs} ${num_warmup_steps} ${N_FOLDS} 0 ${additional_args} ${lr} ${weight_decay} ${seed}
-scripts/run-fractional.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${epochs} ${num_warmup_steps} ${N_FOLDS} 1 ${additional_args} ${lr} ${weight_decay} ${seed}
+scripts/run-fractional.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${fractional_epochs} ${num_warmup_steps} ${N_FOLDS} 0 ${additional_args} ${lr} ${weight_decay} ${seed}
+scripts/run-fractional.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${fractional_epochs} ${num_warmup_steps} ${N_FOLDS} 1 ${additional_args} ${lr} ${weight_decay} ${seed}
 python scripts/combining_dataset_folds.py ${MODEL} ${dataset} ${N_FOLDS}
 scripts/run-combined.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${combined_epochs} ${num_warmup_steps} ${N_FOLDS} ${proba_column} ${proba_dataset} ${additional_args} ${lr} ${weight_decay} ${seed}
 
--- scripts/samsum/run-t5-base.sh
+++ scripts/samsum/run-t5-base.sh
@@ -1,11 +1,12 @@
 MODEL="t5-base"
-bs=16
+bs=32
 lr=2e-4
 weight_decay=0.001
 dataset="samsum"
-gradient_accumulation_steps=4
+gradient_accumulation_steps=2
 baseline_epochs=20
-epochs=16
+fractional_epochs=20
+epochs=15
 combined_epochs=12
 num_warmup_steps=180
 N_FOLDS=2
@@ -16,8 +17,8 @@ seed=37
 
 
 # scripts/run-baseline.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${baseline_epochs} ${num_warmup_steps} ${additional_args} ${lr} ${weight_decay} ${seed}
-# scripts/run-fractional.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${epochs} ${num_warmup_steps} ${N_FOLDS} 0 ${additional_args} ${lr} ${weight_decay} ${seed}
-# scripts/run-fractional.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${epochs} ${num_warmup_steps} ${N_FOLDS} 1 ${additional_args} ${lr} ${weight_decay} ${seed}
+scripts/run-fractional.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${fractional_epochs} ${num_warmup_steps} ${N_FOLDS} 0 ${additional_args} ${lr} ${weight_decay} ${seed}
+scripts/run-fractional.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${fractional_epochs} ${num_warmup_steps} ${N_FOLDS} 1 ${additional_args} ${lr} ${weight_decay} ${seed}
 # python scripts/combining_dataset_folds.py ${MODEL} ${dataset} ${N_FOLDS}
 # scripts/run-combined.sh ${MODEL} ${dataset} ${bs} ${gradient_accumulation_steps} ${combined_epochs} ${num_warmup_steps} ${N_FOLDS} ${proba_column} ${proba_dataset} ${additional_args} ${lr} ${weight_decay} ${seed}
 
